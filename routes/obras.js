e03703e599f489a439c7836baa943862b4a24047
Aline Lima <166144786+AlineL1ma@users.noreply.github.com>
2025-06-16 18:45:22 +0000

    alterando rotas

--- routes/obras.js
+++ routes/obras.js
@@ -16,24 +16,24 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Criar uma nova obra
-router.post('/obras', upload.single('foto'), obraController.createObra);
+router.post('/', upload.single('foto'), obraController.createObra);
 
 // Listar todas as obras
-router.get('/obras', obraController.getObras);
+router.get('/', obraController.getObras);
 
 // Buscar obra por ID
-router.get('/obras/:id', obraController.getObraById);
+router.get('/:id', obraController.getObraById);
 
 // Atualizar obra
-router.put('/obras/:id', upload.single('foto'), obraController.updateObra);
+router.put('/:id', upload.single('foto'), obraController.updateObra);
 
 // Deletar obra
-router.delete('/obras/:id', obraController.deleteObra);
+router.delete('/:id', obraController.deleteObra);
 
 // Detalhes da obra com lista de fiscalizações
-router.get('/obras/:id/detalhes', obraController.getObraDetalhes);
+router.get('/:id/detalhes', obraController.getObraDetalhes);
 
 // Enviar dados da obra e fiscalizações por e-mail
-router.post('/obras/:id/enviar-email', obraController.enviarObraPorEmail);
+router.post('/:id/enviar-email', obraController.enviarObraPorEmail);
 
 module.exports = router;
