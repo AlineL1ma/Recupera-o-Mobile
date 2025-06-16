600ea3fe5394d2bce82dc5183e1ee9e660a5c819
Aline Lima <166144786+AlineL1ma@users.noreply.github.com>
2025-06-16 16:41:12 +0000

    Estruturando o BackEnd

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const obrasRouter = require('./routes/obras');
+const fiscalizacoesRouter = require('./routes/fiscalizacoes');
+const path = require('path');
+const connectDB = require('./config/database');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/obras', obrasRouter);
+app.use('/fiscalizacoes', fiscalizacoesRouter);
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
