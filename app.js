3d44e29f46e19e61bf4219e3bbe269cd651d5aa8
Aline Lima <166144786+AlineL1ma@users.noreply.github.com>
2025-06-16 17:32:17 +0000

    configurando o cors

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const obrasRouter = require('./routes/obras');
 const fiscalizacoesRouter = require('./routes/fiscalizacoes');
 const path = require('path');
 const connectDB = require('./config/database');
+const cors = require('cors');
 
 const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/obras', obrasRouter);
